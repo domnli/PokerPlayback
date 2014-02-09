0a7582438ef7f5eef0cdbd5d7909052bea5e5e0b
董明理 <418005608@qq.com>
2014-02-09 19:49:16 +0800

    服务端xml转json

--- client/iio/PokerPlayback.js
+++ client/iio/PokerPlayback.js
@@ -1,6 +1,7 @@
 //PokerPlayback
 var PokerPlayback = function (io) {
     var playerPositions = [
+        {},
         {name : {x : 330, y : 700}, icon : {x : 330, y : 750}, chips : {x : 330, y : 800}, dealer : {x : 280, y : 680}, blind : {x : 340, y : 680}, bet : {x : 340, y : 650}},
         {name : {x : 80, y : 560}, icon : {x : 80, y : 610}, chips : {x : 80, y : 660}, dealer : {x : 130, y : 600}, blind : {x : 130, y : 630}, bet : {x : 160, y : 630}},
         {name : {x : 80, y : 360}, icon : {x : 80, y : 410}, chips : {x : 80, y : 460}, dealer : {x : 130, y : 400}, blind : {x : 130, y : 430}, bet : {x : 160, y : 430}},
@@ -11,7 +12,15 @@ var PokerPlayback = function (io) {
         {name : {x : 560, y : 360}, icon : {x : 560, y : 410}, chips : {x : 560, y : 460}, dealer : {x : 510, y : 400}, blind : {x : 510, y : 430}, bet : {x : 480, y : 430}},
         {name : {x : 560, y : 560}, icon : {x : 560, y : 610}, chips : {x : 560, y : 660}, dealer : {x : 510, y : 600}, blind : {x : 510, y : 630}, bet : {x : 480, y : 630}}
       ],
-      potPosition = {x : 330, y : 350},
+      playerPositions5 = [
+        {},
+        {name : {x : 330, y : 700}, icon : {x : 330, y : 750}, chips : {x : 330, y : 800}, dealer : {x : 280, y : 680}, blind : {x : 340, y : 680}, bet : {x : 340, y : 650}},
+        {name : {x : 80, y : 360}, icon : {x : 80, y : 410}, chips : {x : 80, y : 460}, dealer : {x : 130, y : 400}, blind : {x : 130, y : 430}, bet : {x : 160, y : 430}},
+        {name : {x : 80, y : 160}, icon : {x : 80, y : 210}, chips : {x : 80, y : 260}, dealer : {x : 130, y : 200}, blind : {x : 130, y : 230}, bet : {x : 160, y : 230}},
+        {name : {x : 560, y : 160}, icon : {x : 560, y : 210}, chips : {x : 560, y : 260}, dealer : {x : 510, y : 200}, blind : {x : 510, y : 230}, bet : {x : 480, y : 230}},
+        {name : {x : 560, y : 360}, icon : {x : 560, y : 410}, chips : {x : 560, y : 460}, dealer : {x : 510, y : 400}, blind : {x : 510, y : 430}, bet : {x : 480, y : 430}},
+      ],
+      potPosition = {x : 325, y : 350},
       boardPositions = [
             {x : 230, y : 450},
             {x : 276, y : 450},
@@ -19,7 +28,10 @@ var PokerPlayback = function (io) {
             {x : 368, y : 450},
             {x : 414, y : 450}
         ];
-    var  i;
+    var  i, record = recordHelper.data, table = record.STAGE.TABLE, seats = table.SEAT;
+    if (table.SEATS == 5) {
+        playerPositions = playerPositions5;
+    }
     io.setBGColor('rgba(0, 126, 255, 0.4)');
     io.addGroup('table'); //头像、名称、筹码组
     /*for (i = 0; i < playerPositions.length; i++) {
@@ -29,63 +41,78 @@ var PokerPlayback = function (io) {
                     .setAlpha(.1)
             );
     };*/
-    for (i = 0; i < playerPositions.length; i++) {
+    for (i = 0; i < seats.length; i++) {
         (function(pos){
-            playerPositions[i].icon.obj = new iio.Circle(playerPositions[i].icon, 30)
+            playerPositions[seats[i].NUMBER].icon.obj = new iio.Circle(playerPositions[seats[i].NUMBER].icon, 30)
                                         .setStrokeStyle('white',2)
-                                        .addImage('res/icon.png', function(){
+                                        .addImage(seats[i].ICON, function(){
                                                                 io.addToGroup('table',playerPositions[pos].icon.obj);
                                                             }); // 头像
-        })(i);
+        })(seats[i].NUMBER);
 
-        playerPositions[i].name.obj = new iio.Text('名称asdfdsafdsa', playerPositions[i].name)
+        playerPositions[seats[i].NUMBER].name.obj = new iio.Text(seats[i].NAME, playerPositions[seats[i].NUMBER].name)
                                     .setFont('14px Microsoft YaHei')
                                     .setTextAlign('center')
                                     .setFillStyle('white'); // 名称
-        io.addToGroup('table', playerPositions[i].name.obj);
+        io.addToGroup('table', playerPositions[seats[i].NUMBER].name.obj);
      /*   playerPositions[i].chips.bgobj = new iio.SimpleRect(playerPositions[i].chips.x, playerPositions[i].chips.y - 5, 80, 20)
                                         .setFillStyle('black')
                                         .setAlpha(.3); // 筹码背景
         io.addToGroup('table', playerPositions[i].chips.bgobj);*/
-        playerPositions[i].chips.obj = new iio.Text('8888.0000', playerPositions[i].chips)
+        playerPositions[seats[i].NUMBER].chips.obj = new iio.Text(seats[i].CHIPS, playerPositions[seats[i].NUMBER].chips)
                                     .setFont('12px Microsoft YaHei')
                                     .setTextAlign('center')
                                     .setFillStyle('white'); // 筹码
-        io.addToGroup('table', playerPositions[i].chips.obj);
+        io.addToGroup('table', playerPositions[seats[i].NUMBER].chips.obj);
+    };
 
-
-        playerPositions[i].dealer.obj = new iio.Text('D', playerPositions[i].dealer)
+    playerPositions[table.DEALER].dealer.obj = new iio.Text('D', playerPositions[table.DEALER].dealer)
                                             .setFont('20px Microsoft YaHei')
                                             .setTextAlign('center')
                                             .setFillStyle('white'); // 庄家标识
-        io.addToGroup('table', playerPositions[i].dealer.obj);
-        playerPositions[i].blind.obj = new iio.Text('SB100', playerPositions[i].blind)
-                                            .setFont('13px Microsoft YaHei')
-                                            .setTextAlign('center')
-                                            .setFillStyle('white'); // 庄家标识
-        io.addToGroup('table', playerPositions[i].blind.obj);
-        playerPositions[i].bet.obj = new iio.Text('bet200', playerPositions[i].bet)
-                                            .setFont('13px Microsoft YaHei')
-                                            .setTextAlign('center')
-                                            .setFillStyle('white'); // 庄家标识
-        io.addToGroup('table', playerPositions[i].bet.obj);
-    };
-    potPosition.obj = new iio.Text('POT10000', potPosition)
+    io.addToGroup('table', playerPositions[table.DEALER].dealer.obj);
+
+    potPosition.obj = new iio.Text('0', potPosition)
                             .setFont('30px Microsoft YaHei')
                             .setTextAlign('center')
                             .setFillStyle('white'); // 奖池
     io.addToGroup('table', potPosition.obj);
-    for (i = 0; i < boardPositions.length; i++) {
+
+    playerPositions[table.SBLIND.NUMBER].blind.obj = new iio.Text('S ' + table.SBLIND.CHIPS, playerPositions[table.SBLIND.NUMBER].blind)
+                                            .setFont('13px Microsoft YaHei')
+                                            .setTextAlign('center')
+                                            .setFillStyle('white'); // 小盲注
+    io.addToGroup('table', playerPositions[table.SBLIND.NUMBER].blind.obj);
+    payChipsAnim(playerPositions[table.SBLIND.NUMBER].blind.obj, playerPositions[table.SBLIND.NUMBER].icon, playerPositions[table.SBLIND.NUMBER].blind);
+
+    playerPositions[table.BBLIND.NUMBER].blind.obj = new iio.Text('B ' + table.BBLIND.CHIPS, playerPositions[table.BBLIND.NUMBER].blind)
+                                            .setFont('13px Microsoft YaHei')
+                                            .setTextAlign('center')
+                                            .setFillStyle('white'); // 大盲注
+    io.addToGroup('table', playerPositions[table.BBLIND.NUMBER].blind.obj);
+    payChipsAnim(playerPositions[table.BBLIND.NUMBER].blind.obj, playerPositions[table.BBLIND.NUMBER].icon, playerPositions[table.BBLIND.NUMBER].blind);
+    /*
+    for (i = 0; i < boardPositions.length; i++) { //牌面
         (function(pos){
             boardPositions[i].obj = new iio.SimpleRect(boardPositions[i], 45, 65)
                                     .addImage('res/Ad.png', function(){
                                                 io.addToGroup('table',boardPositions[pos].obj);
                                             });
         })(i);
-    };
+    };*/
+    function payChipsAnim(obj, from, to) { //从 pos.x - x, pox.y - y移动到pos
+        obj.setPos(from);
+        obj.enableKinematics().setVel(from.x == to.x ? 0 : from.x > to.x ? -2 : 2, from.y == to.y ? 0 : from.y > to.y ? -2 : 2);
+        io.setFramerate(60, function(){
+            if (to.x == obj.pos.x && to.y == obj.pos.y) {
+                io.cancelFramerate();
+            }
+        });
+    }
     console.log(io);
+    recordHelper.io = io;
   };
 var recordHelper = {
-    json : {},
+    data : {},
 
 };
---
+++ server/poker.php
@@ -0,0 +1,92 @@
+<?php
+class Poker {
+	public $xmlObject;
+	public $xmlString;
+	public function __construct($xmlFileName) {
+		if (file_exists($xmlFileName)) {
+			$this->xmlObject = simplexml_load_file($xmlFileName);
+			$this->xmlString = file_get_contents($xmlFileName);
+			if ($this->xmlObject === false) {
+				throw new Exception('not xml file');
+			}
+		}else{
+			throw new Exception('file not found');
+		}
+	}
+
+
+	private function parsePOKERCARD() {
+		preg_match("/<POKERCARD>(.*)<\/POKERCARD>/s", $this->xmlString, $pokercard);
+		preg_match("/<POKERCARD>(.*)<FLOP>/s", $pokercard[0], $preflop);
+		preg_match("/<\/FLOP>(.*)<TURN>/s", $pokercard[0], $flop);
+		preg_match("/<\/TURN>(.*)<RIVER>/s", $pokercard[0], $turn);
+		preg_match("/<\/RIVER>(.*)<\/POKERCARD>/s", $pokercard[0], $river);
+		//print_r($pokercard);
+		$countPreflop = preg_match_all("/<PLAYER>/", $preflop[0]);
+		$countFlop = preg_match_all("/<PLAYER>/", $flop[0]);
+		$countTurn = preg_match_all("/<PLAYER>/", $turn[0]);
+		$countRiver = preg_match_all("/<PLAYER>/", $river[0]);
+		//print_r($flop);
+		//echo ($flop[0])
+		$noseq = json_decode(json_encode($this->xmlObject->STAGE->POKERCARD),true);
+		$playerPool = $noseq['PLAYER'];
+		$export = array(
+						'HOLECARD' => $noseq['HOLECARD'],
+						'PREFLOP' => array('PLAYER' => array()),
+						'FLOP' => array(
+										'CARD' => $noseq['FLOP']['CARD'],
+										'POT' => $noseq['FLOP']['POT'],
+										'PLAYER' => array()
+									),
+						'TURN' => array(
+										'CARD' => $noseq['TURN']['CARD'],
+										'POT' => $noseq['TURN']['POT'],
+										'PLAYER' => array()
+									),
+						'RIVER' => array(
+										'CARD' => $noseq['RIVER']['CARD'],
+										'POT' => $noseq['RIVER']['POT'],
+										'PLAYER' => array()
+									),
+					);
+		//print_r($export);
+		//echo count($playerPool);
+		$export['PREFLOP']['PLAYER'] = array_splice($playerPool, 0, $countPreflop);
+		$export['FLOP']['PLAYER'] = array_splice($playerPool, 0, $countFlop);
+		$export['TURN']['PLAYER'] = array_splice($playerPool, 0, $countTurn);
+		$export['RIVER']['PLAYER'] = array_splice($playerPool, 0, $countRiver);
+		//echo count($playerPool);
+		//print_r($export);
+		return $export;
+	}
+
+	public function exportJSON() {
+		/*foreach ($this->xmlObject as $key => $value) {
+			echo "$key".'<br/>----------------------------------------------';
+			var_dump($value);
+		}*/
+/*		$pokerArr = array(
+						'stage' => array(
+										'title' => $this->xmlObject->STAGE->TITLE,
+										'id' => $this->xmlObject->STAGE->ID,
+										'time' => $this->xmlObject->STAGE->TIME,
+										'table' => array(),
+										'pokercard' => array(
+															'holecard' => array(),
+															'preflop' => array(),
+															'flop' => array(),
+															'turn' => array(),
+															'river' => array()
+														),
+										'showdown' => array()
+									)
+					);*/
+		$pokerArr = json_decode(json_encode($this->xmlObject),true);
+		$pokerArr['STAGE']['POKERCARD'] = $this->parsePOKERCARD();
+		//print_r($pokerArr);
+		return json_encode($pokerArr);
+	}
+}
+
+$poker = new Poker('handhistory.xml');
+echo $poker->exportJSON();
