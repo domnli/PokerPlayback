8f0913df27a62e5b5ee9262479dac8cb5fc35314
domnli <dml8880596@gmail.com>
2015-09-25 15:49:55 +0800

    resource table

--- client/js/resource.js
+++ client/js/resource.js
@@ -3,21 +3,24 @@ var Resource = {
 		this.sounds = {};
 		this.images = {};
 
-		var loader = new iio.Loader(config.basepath);
+		var loader = new iio.Loader(config.resourcepath);
 		function loadSounds(){
-			this.sounds = loader.load([{id:'cardopen',name:'audio/card_open.ogg'},
-										{id:'chipthrow',name:'audio/chip_throw.ogg'},
-										{}],function(res){
-				loadImages();
-			});
-		}.bind(this);
+			this.sounds = loader.load(
+				[{id:'cardopen',name:'audio/card_open.ogg'},
+				{id:'chipthrow',name:'audio/chip_throw.ogg'},
+				],
+			function(res){
+				console.log('sounds has loaded')
+				loadImages.call(this);
+			}.bind(this));
+		};
 
 		function loadImages(){
-			this.images = loader.load([],function(res){
+			this.images = loader.load([{id:'2c',name:'2c.png'}],function(res){
 				console.log('all res has loaded');
 			});
-		}.bind(this);
+		};
 		
-		loadSounds();
+		loadSounds.call(this);
 	}
 };
