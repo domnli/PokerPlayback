c86aaf33c842de240de2d8369381f50b543ade21
domnli <dml8880596@gmail.com>
2015-09-29 08:38:13 +0800

    add player class

---
+++ client/js/player.js
@@ -0,0 +1,6 @@
+function Player(conf){
+	this.chips = conf.chips||conf.CHIPS||0;
+	this.icon = conf.icon||conf.ICON||0;
+	this.name = conf.name||conf.NAME||0;
+	this.number = conf.number||conf.NUMBER||0;
+}
--- client/js/table.js
+++ client/js/table.js
@@ -1,1 +1,9 @@
-var Table = {};
+var Table = {
+	init:function(table){
+		this.dealer = table.dealer||table.DEALER;
+		this.currency table.currency||table.CURRENCY;
+		this.players = [];
+		// todo init players
+		
+	}
+};
